9044e5c2e916d425ba3ce0f6cfabf3b964c04229
JuanMend <juanmend690@gmail.com>
2019-09-26 23:16:14 -0700

    done with footer for now

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import InstagramIcon from '@material-ui/icons/Instagram';
-
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
 const useStyles = makeStyles((theme) => ({
 	root: {
-		height: '50vh',
+		height: '35vh',
 		backgroundColor: 'black'
 	},
 	ulList: {
@@ -13,7 +14,7 @@ const useStyles = makeStyles((theme) => ({
 		listStyleType: 'none',
 		fontSize: '16px',
 		lineHeight: '30px',
-		border: 'solid red',
+		// border: 'solid pink',
 		justifyContent: 'center',
 		alignItems: 'center',
 		display: 'flex',
@@ -23,8 +24,23 @@ const useStyles = makeStyles((theme) => ({
 		padding: '50px'
 	},
 	mainDivUl: {
-		border: 'solid red',
-		width: '100%'
+		// border: 'solid orange',
+		width: '50%'
+	},
+	iconsUl: {
+		// border: 'solid red',
+		width: '40%'
+	},
+	contDiv: {
+		// border: 'solid blue',
+		marginTop: '10vh',
+		width: '100%',
+		justifyContent: 'center',
+		alignItems: 'flex-start',
+		display: 'flex'
+	},
+	ulListIcon: {
+		listStyleType: 'none'
 	}
 }));
 
@@ -33,19 +49,27 @@ export default function Footer() {
 	const theme = useTheme();
 	return (
 		<Grid container className={classes.root}>
-			<div className={classes.mainDivUl}>
-				<ul className={classes.ulList}>
-					<li>CONTACT</li>
-					<li>ABOUT US</li>
-					<li>HOME</li>
-				</ul>
-			</div>
-			<div>
-				<ul>
-					<li />
-					<li />
-					<li />
-				</ul>
+			<div className={classes.contDiv}>
+				<div className={classes.mainDivUl}>
+					<ul className={classes.ulList}>
+						<li>CONTACT</li>
+						<li>ABOUT US</li>
+						<li>HOME</li>
+					</ul>
+				</div>
+				<div className={classes.iconsUl}>
+					<ul className={classes.ulListIcon}>
+						<li>
+							<InstagramIcon style={{ color: 'white', fontSize: '30px' }} />
+						</li>
+						<li>
+							<FacebookIcon style={{ color: 'white', fontSize: '30px' }} />
+						</li>
+						<li>
+							<TwitterIcon style={{ color: 'white', fontSize: '30px' }} />
+						</li>
+					</ul>
+				</div>
 			</div>
 		</Grid>
 	);
