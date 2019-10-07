a8f428904cdb3e5b5496740dcb9808545ea70d70
JuanMend <juanmend690@gmail.com>
2019-10-07 11:53:00 -0700

    recipe

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
+import './Recipes.css';
 
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
+import Grid from '@material-ui/core/Grid';
 // import tileData from './tileData';
 
 import ButtonBase from '@material-ui/core/ButtonBase';
@@ -20,6 +21,21 @@ import Cupcakes from '../../Pics/cupcakes.png';
 import Pizza from '../../Pics/pizza.png';
 import Healthy from '../../Pics/healthy.png';
 import Chicken from '../../Pics/chicken.png';
+
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const images = [
 	{
@@ -143,11 +159,37 @@ const useStyles = makeStyles((theme) => ({
 		bottom: -2,
 		left: 'calc(50% - 9px)',
 		transition: theme.transitions.create('opacity')
+	},
+	card: {
+		maxWidth: 325,
+		marginTop: '10vh'
+	},
+	media: {
+		height: 0,
+		paddingTop: '56.25%' // 16:9
+	},
+	expand: {
+		transform: 'rotate(0deg)',
+		marginLeft: 'auto',
+		transition: theme.transitions.create('transform', {
+			duration: theme.transitions.duration.shortest
+		})
+	},
+	expandOpen: {
+		transform: 'rotate(180deg)'
+	},
+	avatar: {
+		backgroundColor: red[500]
 	}
 }));
 
 export default function Recipes() {
 	const classes = useStyles();
+	const [ expanded, setExpanded ] = React.useState(false);
+
+	const handleExpandClick = () => {
+		setExpanded(!expanded);
+	};
 	// const theme = useTheme();
 
 	return (
@@ -187,7 +229,225 @@ export default function Recipes() {
 					))}
 				</GridList>
 			</div>
-			{/* <Footer /> */}
+			<Grid>
+				<div className="mainCard">
+					<Card className={classes.card}>
+						<CardHeader
+							action={
+								<IconButton aria-label="settings">
+									<MoreVertIcon />
+								</IconButton>
+							}
+							title="Pizza"
+						/>
+						<CardMedia className={classes.media} image={Pizza} title="Paella dish" />
+						<CardContent>
+							<Typography variant="body2" color="textSecondary" component="p">
+								This impressive paella is a perfect party dish and a fun meal to cook together with your
+								guests. Add 1 cup of frozen peas along with the mussels, if you like.
+							</Typography>
+						</CardContent>
+						<CardActions disableSpacing>
+							<IconButton aria-label="add to favorites">
+								<FavoriteIcon />
+							</IconButton>
+
+							<IconButton
+								className={clsx(classes.expand, {
+									[classes.expandOpen]: expanded
+								})}
+								onClick={handleExpandClick}
+								aria-expanded={expanded}
+								aria-label="show more"
+							>
+								<ExpandMoreIcon />
+							</IconButton>
+						</CardActions>
+						<Collapse in={expanded} timeout="auto" unmountOnExit>
+							<CardContent>
+								<Typography paragraph>Recipe:</Typography>
+								<Typography paragraph>
+									Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+									minutes.
+								</Typography>
+							</CardContent>
+						</Collapse>
+					</Card>
+
+					<Card className={classes.card}>
+						<CardHeader
+							action={
+								<IconButton aria-label="settings">
+									<MoreVertIcon />
+								</IconButton>
+							}
+							title="Pizza"
+						/>
+						<CardMedia className={classes.media} image={Chicken} title="Paella dish" />
+						<CardContent>
+							<Typography variant="body2" color="textSecondary" component="p">
+								This impressive paella is a perfect party dish and a fun meal to cook together with your
+								guests. Add 1 cup of frozen peas along with the mussels, if you like.
+							</Typography>
+						</CardContent>
+						<CardActions disableSpacing>
+							<IconButton aria-label="add to favorites">
+								<FavoriteIcon />
+							</IconButton>
+
+							<IconButton
+								className={clsx(classes.expand, {
+									[classes.expandOpen]: expanded
+								})}
+								onClick={handleExpandClick}
+								aria-expanded={expanded}
+								aria-label="show more"
+							>
+								<ExpandMoreIcon />
+							</IconButton>
+						</CardActions>
+						<Collapse in={expanded} timeout="auto" unmountOnExit>
+							<CardContent>
+								<Typography paragraph>Recipe:</Typography>
+								<Typography paragraph>
+									Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+									minutes.
+								</Typography>
+							</CardContent>
+						</Collapse>
+					</Card>
+
+					<Card className={classes.card}>
+						<CardHeader
+							action={
+								<IconButton aria-label="settings">
+									<MoreVertIcon />
+								</IconButton>
+							}
+							title="Pizza"
+						/>
+						<CardMedia className={classes.media} image={Sushi} title="Paella dish" />
+						<CardContent>
+							<Typography variant="body2" color="textSecondary" component="p">
+								This impressive paella is a perfect party dish and a fun meal to cook together with your
+								guests. Add 1 cup of frozen peas along with the mussels, if you like.
+							</Typography>
+						</CardContent>
+						<CardActions disableSpacing>
+							<IconButton aria-label="add to favorites">
+								<FavoriteIcon />
+							</IconButton>
+
+							<IconButton
+								className={clsx(classes.expand, {
+									[classes.expandOpen]: expanded
+								})}
+								onClick={handleExpandClick}
+								aria-expanded={expanded}
+								aria-label="show more"
+							>
+								<ExpandMoreIcon />
+							</IconButton>
+						</CardActions>
+						<Collapse in={expanded} timeout="auto" unmountOnExit>
+							<CardContent>
+								<Typography paragraph>Recipe:</Typography>
+								<Typography paragraph>
+									Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+									minutes.
+								</Typography>
+							</CardContent>
+						</Collapse>
+					</Card>
+
+					<Card className={classes.card}>
+						<CardHeader
+							action={
+								<IconButton aria-label="settings">
+									<MoreVertIcon />
+								</IconButton>
+							}
+							title="Pizza"
+						/>
+						<CardMedia className={classes.media} image={Cupcakes} title="Paella dish" />
+						<CardContent>
+							<Typography variant="body2" color="textSecondary" component="p">
+								This impressive paella is a perfect party dish and a fun meal to cook together with your
+								guests. Add 1 cup of frozen peas along with the mussels, if you like.
+							</Typography>
+						</CardContent>
+						<CardActions disableSpacing>
+							<IconButton aria-label="add to favorites">
+								<FavoriteIcon />
+							</IconButton>
+
+							<IconButton
+								className={clsx(classes.expand, {
+									[classes.expandOpen]: expanded
+								})}
+								onClick={handleExpandClick}
+								aria-expanded={expanded}
+								aria-label="show more"
+							>
+								<ExpandMoreIcon />
+							</IconButton>
+						</CardActions>
+						<Collapse in={expanded} timeout="auto" unmountOnExit>
+							<CardContent>
+								<Typography paragraph>Recipe:</Typography>
+								<Typography paragraph>
+									Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+									minutes.
+								</Typography>
+							</CardContent>
+						</Collapse>
+					</Card>
+
+					<Card className={classes.card}>
+						<CardHeader
+							action={
+								<IconButton aria-label="settings">
+									<MoreVertIcon />
+								</IconButton>
+							}
+							title="Pizza"
+						/>
+						<CardMedia className={classes.media} image={Tacos} title="Paella dish" />
+						<CardContent>
+							<Typography variant="body2" color="textSecondary" component="p">
+								This impressive paella is a perfect party dish and a fun meal to cook together with your
+								guests. Add 1 cup of frozen peas along with the mussels, if you like.
+							</Typography>
+						</CardContent>
+						<CardActions disableSpacing>
+							<IconButton aria-label="add to favorites">
+								<FavoriteIcon />
+							</IconButton>
+
+							<IconButton
+								className={clsx(classes.expand, {
+									[classes.expandOpen]: expanded
+								})}
+								onClick={handleExpandClick}
+								aria-expanded={expanded}
+								aria-label="show more"
+							>
+								<ExpandMoreIcon />
+							</IconButton>
+						</CardActions>
+						<Collapse in={expanded} timeout="auto" unmountOnExit>
+							<CardContent>
+								<Typography paragraph>Recipe:</Typography>
+								<Typography paragraph>
+									Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+									minutes.
+								</Typography>
+							</CardContent>
+						</Collapse>
+					</Card>
+				</div>
+			</Grid>
+			<Footer />
 		</div>
 	);
 }
